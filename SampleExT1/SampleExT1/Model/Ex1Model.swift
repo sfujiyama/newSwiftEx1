78d33c123ed6639447ae34f18956a5c5fd0def72
Soseki Fujiyama <sin.fukase.nine@gmail.com>
2021-12-05 13:16:17 +0900

    custom

---
+++ SampleExT1/SampleExT1/Model/Ex1Model.swift
@@ -0,0 +1,26 @@
+//
+//  Ex1Model.swift
+//  SampleExT1
+//
+//  Created by pleocene on 2021/12/05.
+//
+
+import Foundation
+
+class Ex1Model {
+    init() {
+        print("init")
+    }
+    
+    func funcExT1(){
+        print("funcExT1")
+    }
+    func funcExT2(){
+        print("funcExT1")
+    }
+    func funcExT3(){
+        print("funcExT1")
+    }
+    
+}
+
