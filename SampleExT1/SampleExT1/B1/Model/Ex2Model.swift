7a02e38c52f1d60242ca0a72ac06bf777efe42e6
S.Fujiyama <ssfujiyama2020@gmail.com>
2021-12-07 08:14:32 +0900

    修正

---
+++ SampleExT1/SampleExT1/B1/Model/Ex2Model.swift
@@ -0,0 +1,16 @@
+//
+//  Ex2model.swift
+//  SampleExT1
+//
+//  Created by pleocene on 2021/12/07.
+//
+
+import Foundation
+
+class Ex2Model {
+    var string1 = ""
+    var string2 = ""
+    var string3 = ""
+
+}
+
