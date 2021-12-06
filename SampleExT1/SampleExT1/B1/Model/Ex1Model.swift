6fcfd3e3b32525c63277abbfccdf10e8804a0057
S.Fujiyama <ssfujiyama2020@gmail.com>
2021-12-07 07:55:07 +0900

    edit

--- SampleExT1/SampleExT1/B1/Model/Ex1Model.swift
+++ SampleExT1/SampleExT1/B1/Model/Ex1Model.swift
@@ -9,17 +9,15 @@ import Foundation
 
 class Ex1Model {
     init() {
-        debugPrint("dev1_C>add")
-        debugPrint("dev1_A>add")
-        debugPrint("devT1_A>test")
-        print("init > test kraken")
+        debugPrint("NEW")
+        print("init > test kraken>NEW")
     }
     
     func funcExT1(){
-        print("funcExT1 extend > after rebart eidt>")
+        print("funcExT1 extend > aftexxxxxxr rebart eidt>")
     }
     func funcExT2(){
-        print("funcExT1>dev1_C>Edit＞修正")
+        print("funcExT1>dev1_C>Edit＞修sssss正")
     }
     func funcExT3(){
         print("funcExT1")
